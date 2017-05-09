e78a8c6bbe056a3bd283cb054a8cb305f4c9ead9
ALDER HERNANDEZ <analista.guma@gmail.com>
2017-05-09 17:45:58 -0600

    09-05-2017 alder

--- application/controllers/Servicio_controllers.php
+++ application/controllers/Servicio_controllers.php
@@ -68,3 +68,53 @@ class Servicio_controllers extends CI_Controller {
 		$this->servicios_model->updatePedidos($_POST['PEDIDOS']);
 	}
 }
+/*[
+	{"detalles":
+		{"nameValuePairs":
+			{
+				"ID0":"F09P03051731",
+				"ARTICULO0":"10126011",
+				"DESC0":"Leucovorina Cálcica 50 mg/5 ml Solución Iny. 5 ml/Vial 1/Caja Refrigerado (Naprod)",
+				"CANT0":"150",
+				"TOTAL0":"190.00",
+				"BONI0":"0",
+				"ID1":"F09P03051731",
+				"ARTICULO1":"10301052",
+				"DESC1":"Acetaminofén 300 mg Supositorios 10/Caja (Ramos)",
+				"CANT1":"20",
+				"TOTAL1":"49.50",
+				"BONI1":"5+1",
+				"ID2":"F09P03051731",
+				"ARTICULO2":"10303022",
+				"DESC2":"Ambroxol + Clenbuterol 7.5 mg x 0.005 mg/5 ml Jarabe 120 ml/Frasco 1/Caja (Ramos)",
+				"CANT2":"3",
+				"TOTAL2":"55.00",
+				"BONI2":"0"
+			}},"mCliente":"02319","mEstado":"0","mFecha":"2017-05-03 16:52:53","mIdPedido":"F09P03051731","mNombre":"FARMACIA DRA ZELEDON","mPrecio":"29655.0","mVendedor":"F09"},
+	{"detalles":
+		{"nameValuePairs":
+			{
+				"ID3":"F09P03051732",
+				"ARTICULO3":"10118022",
+				"DESC3":"Anastrozol 1 Mg Tab Recubierta 28/Caja (Naprod)",
+				"CANT3":"1",
+				"TOTAL3":"1066.00",
+				"BONI3":"0"
+			}},"mCliente":"01762","mEstado":"0","mFecha":"2017-05-03 18:01:30","mIdPedido":"F09P03051732","mNombre":"FARMACIA IRELA SOMOTO","mPrecio":"1066.0","mVendedor":"F09"},
+	{"detalles":
+		{"nameValuePairs":
+			{
+				"ID4":"F09P03051733",
+				"ARTICULO4":"10115012",
+				"DESC4":"Lenalidomida 25 mg Capsulas 10/Frasco 1/Caja (Naprod)",
+				"CANT4":"1","TOTAL4":"78000.00","BONI4":"0"
+			}},"mCliente":"00758","mEstado":"0","mFecha":"2017-05-03 18:02:25","mIdPedido":"F09P03051733","mNombre":"FARMACIA GUADALUPE #2","mPrecio":"78000.0","mVendedor":"F09"},
+	{"detalles":
+		{"nameValuePairs":
+			{
+				"ID5":"F09P03051734",
+				"ARTICULO5":"10118441",
+				"DESC5":"Pemetrexed 500 mg Liofilizado para Sol. Iny. Vial 1/Caja (Naprod)",
+				"CANT5":"3","TOTAL5":"19975.00","BONI5":"0"
+			}},"mCliente":"01762","mEstado":"0","mFecha":"2017-05-03 18:03:46","mIdPedido":"F09P03051734","mNombre":"FARMACIA IRELA SOMOTO","mPrecio":"59925.0","mVendedor":"F09"}
+]*/
--- application/models/servicios_model.php
+++ application/models/servicios_model.php
@@ -41,10 +41,18 @@ class servicios_model extends CI_Model
             $rtnCliente['results'][$i]['mSaldo']        = number_format($key['SALDO'],2, '.', '');
             $rtnCliente['results'][$i]['mDisponible']   = number_format($key['DISPONIBLE'],2, '.', '');
             $rtnCliente['results'][$i]['mCumple']       = $this->Cumple($key['CLIENTE']);
+            
             $i++;
         }
         echo json_encode($rtnCliente);
         $this->sqlsrv->close();
+    }
+
+    public function porcentaje($actual,$meta)
+    {
+        if ($meta != 0) {
+            return ($actual/$meta)*100;
+        }return 0;
     }
 
     public function Historial($Vendedor)
@@ -108,6 +116,7 @@ class servicios_model extends CI_Model
             $rtnCliente['results'][$i]['mVentasActual']      = number_format($key['VentaEnValoresAct'],2,'.','');
             $rtnCliente['results'][$i]['mPromedioVenta3M']   = number_format($key['VentaEnValores3MAnt'],2,'.','');
             $rtnCliente['results'][$i]['mCantidadItems3M']   = number_format($key['NumItemFac3MAnt'],2,'.','');
+            $rtnCliente['results'][$i]['mCumplimiento'] = $this->porcentaje($key['VentaEnValoresAct'],$key['MetaVentaEnValores']);
             $i++;
         }
         echo json_encode($rtnCliente);
@@ -247,34 +256,39 @@ class servicios_model extends CI_Model
         echo json_encode($query);
     }
 
-    public function insertPedidos($Data)
-    {
+    public function insertPedidos($Data){
+        
         $i = 0;
         $rtnUsuario = array();
-        //$consulta = "";
+        
         foreach(json_decode($Data, true) as $key){
             
             $query = $this->db->query("SELECT IDPEDIDO FROM pedido WHERE IDPEDIDO = '".$key['mIdPedido']."'");
+            
             if ($query->num_rows() == 0) {
-                    $rtnUsuario['results'][$i]['mEstado'] = $this->db->query('CALL SP_pedidos ("'.$key['mIdPedido'].'","'.$key['mVendedor'].'","'.$key['mCliente'].'",
+                    $rtnUsuario['results'][0]['mEstado'] = $this->db->query('CALL SP_pedidos ("'.$key['mIdPedido'].'","'.$key['mVendedor'].'","'.$key['mCliente'].'",
                                             "'.$key['mNombre'].'","'.$key['mFecha'].'","'.$key['mPrecio'].'","'.$key['mEstado'].'")');
 
-               for ($e=0; $e <(count($key['detalles']['nameValuePairs']))/6; $e++){
-                    $datos = array('IDPEDIDO' => $key['detalles']['nameValuePairs']['ID'.$i],
-                                   'ARTICULO' => $key['detalles']['nameValuePairs']['ARTICULO'.$i],
-                                   'DESCRIPCION' => $key['detalles']['nameValuePairs']['DESC'.$i],
-                                   'CANTIDAD' => $key['detalles']['nameValuePairs']['CANT'.$i],
-                                   'TOTAL' => number_format($key['detalles']['nameValuePairs']['TOTAL'.$i],2),
+                for ($e=0; $e <(count($key['detalles']['nameValuePairs']))/6; $e++){
+                    $datos = array('IDPEDIDO'   => $key['detalles']['nameValuePairs']['ID'.$i],
+                                   'ARTICULO'   => $key['detalles']['nameValuePairs']['ARTICULO'.$i],
+                                   'DESCRIPCION'=> $key['detalles']['nameValuePairs']['DESC'.$i],
+                                   'CANTIDAD'   => $key['detalles']['nameValuePairs']['CANT'.$i],
+                                   'TOTAL'      => number_format($key['detalles']['nameValuePairs']['TOTAL'.$i],2),
                                    'BONIFICADO' => $key['detalles']['nameValuePairs']['BONI'.$i]
                                 );
-                    $rtnUsuario['results'][$i]['mEstado'] = $this->db->insert('pedido_detalle',$datos);
+                    $rtnUsuario['results'][0]['mEstado'] = $this->db->insert('pedido_detalle',$datos);                    
                     $i++;
                 }
             }else{
-                $rtnUsuario['results'][$i]['mEstado'] = "ALGUNOS PEDIDOS NO SE ENVIARON PORQUE YA FUERON REGISTRADOS";
+                for ($e=0; $e <(count($key['detalles']['nameValuePairs']))/6; $e++){
+                    $i++;
+                }
+            }
+            if ($rtnUsuario['results'][0]['mEstado'] = "") {
+                $rtnUsuario['results'][0]['mEstado'] = "ALGUNOS PEDIDOS NO SE PUEDEN REENVIAR YA QUE ESTAN REGISTRADOS";
             }
         }
-        
         echo json_encode($rtnUsuario);
     }
     
