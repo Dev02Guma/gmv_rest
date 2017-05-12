05dcc71d821979d07e0c5b749d9be9e051d636ce
ALDER HERNANDEZ <analista.guma@gmail.com>
2017-05-12 10:54:01 -0600

    12-05-2017 alder

--- application/models/servicios_model.php
+++ application/models/servicios_model.php
@@ -260,34 +260,38 @@ class servicios_model extends CI_Model
         
         $i = 0;
         $rtnUsuario = array();
+        $cadena = "";
         
         foreach(json_decode($Data, true) as $key){
             
             $query = $this->db->query("SELECT IDPEDIDO FROM pedido WHERE IDPEDIDO = '".$key['mIdPedido']."'");
-            
-            if ($query->num_rows() == 0) {
-                    $rtnUsuario['results'][0]['mEstado'] = $this->db->query('CALL SP_pedidos ("'.$key['mIdPedido'].'","'.$key['mVendedor'].'","'.$key['mCliente'].'",
+            $cadena = "'".$key['mIdPedido']."',";
+            if ($query->num_rows() == 0){
+                    $insert = $this->db->query('CALL SP_pedidos ("'.$key['mIdPedido'].'","'.$key['mVendedor'].'","'.$key['mCliente'].'",
                                             "'.$key['mNombre'].'","'.$key['mFecha'].'","'.$key['mPrecio'].'","'.$key['mEstado'].'")');
-
+                    
                 for ($e=0; $e <(count($key['detalles']['nameValuePairs']))/6; $e++){
                     $datos = array('IDPEDIDO'   => $key['detalles']['nameValuePairs']['ID'.$i],
                                    'ARTICULO'   => $key['detalles']['nameValuePairs']['ARTICULO'.$i],
                                    'DESCRIPCION'=> $key['detalles']['nameValuePairs']['DESC'.$i],
                                    'CANTIDAD'   => $key['detalles']['nameValuePairs']['CANT'.$i],
-                                   'TOTAL'      => number_format($key['detalles']['nameValuePairs']['TOTAL'.$i],2),
+                                   'TOTAL'      => number_format(str_replace(",", "", $key['detalles']['nameValuePairs']['TOTAL'.$i]),2),
                                    'BONIFICADO' => $key['detalles']['nameValuePairs']['BONI'.$i]
                                 );
-                    $rtnUsuario['results'][0]['mEstado'] = $this->db->insert('pedido_detalle',$datos);                    
+                    $this->db->insert('pedido_detalle',$datos);                    
                     $i++;
                 }
             }else{
                 for ($e=0; $e <(count($key['detalles']['nameValuePairs']))/6; $e++){
                     $i++;
                 }
-            }
-            if ($rtnUsuario['results'][0]['mEstado'] = "") {
-                $rtnUsuario['results'][0]['mEstado'] = "ALGUNOS PEDIDOS NO SE PUEDEN REENVIAR YA QUE ESTAN REGISTRADOS";
-            }
+            }            
+        }
+        $query = $this->db->query("SELECT IDPEDIDO FROM pedido WHERE IDPEDIDO IN (".substr($cadena, 0,-1).")");
+        if ($query->num_rows()>0) {
+            $rtnUsuario['results'][0]['mEstado'] = "PEDIDOS ENVIADOS...";
+        }else{
+            $rtnUsuario['results'][0]['mEstado'] = "ERROR, INTENTELO MAS TARDE";
         }
         echo json_encode($rtnUsuario);
     }
